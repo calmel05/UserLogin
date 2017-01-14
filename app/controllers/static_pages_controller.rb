ab689e6ebb542eaa083dbda3721c17223ff72a68
Calvin <calmel04@gmail.com>
2017-01-13 23:44:30 -0800

    Changing db configuration to cope with Heroku
    added necessary gems and modified db yml file

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  
+  def home
+    @user = User.all
+  end
+  
+ 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   devise_for :user, only: []
-  
+
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
-    resource :users, only: [:create], controller: :users
+    resources :users, controller: :users
   end
 end
